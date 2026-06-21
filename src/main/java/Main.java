7f2f3f561bb096170429dd6e57c25737fc99adc1
Annav Wadhwa <annav9tech@gmail.com>
2026-06-21 21:14:18 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,10 @@
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-       while(true){
-        System.out.print("$ ");
+System.out.print("$ ");
         String command = scanner.nextLine();
+
+        if (command.equals("exit 0")){
+            break;
+        }
 
         System.out.println(command + ": command not found");
        }
     }
-}

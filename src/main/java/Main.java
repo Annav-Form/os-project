4195a52f3578c8ccdb0a013550f871483385d790
Annav Wadhwa <annav9tech@gmail.com>
2026-06-21 21:44:00 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,58 +1,8 @@
-import java.io.File;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        while (true) {
-            System.out.print("$ ");
-            System.out.flush();
-
-            String command = scanner.nextLine();
-
-            if (command.equals("exit") || command.equals("exit 0")) {
-                break;
-            }
-
-            if (command.startsWith("echo ")) {
-                System.out.println(command.substring(5));
-                continue;
-            }
-
-            if (command.startsWith("type ")) {
-                String cmd = command.substring(5);
-
-                if (cmd.equals("echo") || cmd.equals("exit") || cmd.equals("type")) {
-                    System.out.println(cmd + " is a shell builtin");
-                    continue;
-                }
-
-                String pathEnv = System.getenv("PATH");
-                String[] paths = pathEnv.split(File.pathSeparator);
-
-                boolean found = false;
-
-                for (String path : paths) {
-                    File file = new File(path, cmd);
-
-                    if (file.exists() && file.canExecute()) {
-                        System.out.println(cmd + " is " + file.getAbsolutePath());
-                        found = true;
-                        break;
-                    }
-                }
-
-                if (!found) {
-                    System.out.println(cmd + ": not found");
-                }
-
-                continue;
-            }
-
-            System.out.println(command + ": command not found");
-        }
-
-        scanner.close();
+    public static void main(String[] args) throws Exception {
+        // TODO: Uncomment the code below to pass the first stage
+        System.out.print("$ ");
     }
 }

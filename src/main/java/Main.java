02fb21fb72a819e359190e72fe8bc74b667689a9
Annav Wadhwa <annav9tech@gmail.com>
2026-06-21 21:17:50 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,10 @@
-System.out.print("$ ");
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+       while(true){
+        System.out.print("$ ");
         String command = scanner.nextLine();
 
         if (command.equals("exit 0")){
